60e7f5e47094589f0ceea4b201c4610eca10616c
bruno donatelli <bruno.donatelli@etec.sp.gov.br>
2022-01-10 16:48:02 -0300

    Refactoring and add an architeture scalable to project

--- gatitobook/src/app/modules/home/login/login.component.ts
+++ gatitobook/src/app/modules/home/login/login.component.ts
@@ -1,4 +1,4 @@
-import { AuthService } from './../../authentication/auth.service';
+import { AuthService } from './../../../core/authentication/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
